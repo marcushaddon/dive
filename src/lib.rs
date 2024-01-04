0ab612a2734148e841f7bcfdc764a4e1ed67cf40
marcushaddon <haddon.marcus@gmail.com>
2024-01-04 11:19:34 -0800

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -58,27 +58,17 @@ impl Default for Whammy {
 impl Default for WhammyParams {
 	fn default() -> Self {
 		Self {
-			// This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-			// to treat these kinds of parameters as if we were dealing with decibels. Storing this
-			// as decibels is easier to work with, but requires a conversion for every sample.
 			dive: FloatParam::new(
 				"Div",
 				0.,
 				FloatRange::Skewed {
 					min: -1.,
 					max: 0.,
-					// This makes the range appear as if it was linear when displaying the values as
-					// decibels
 					factor: 1.
 				},
 			)
-			// Because the gain parameter is stored as linear gain instead of storing the value as
-			// decibels, we need logarithmic smoothing
 			.with_smoother(SmoothingStyle::Logarithmic(50.0))
 			.with_unit(" dB")
-			// There are many predefined formatters we can use here. If the gain was stored as
-			// decibels instead of as a linear gain value, we could have also used the
-			// `.with_step_size(0.1)` function to get internal rounding.
 			.with_value_to_string(formatters::v2s_f32_gain_to_db(2))
 			.with_string_to_value(formatters::s2v_f32_gain_to_db()),
 		}
@@ -93,8 +83,6 @@ impl Plugin for Whammy {
 
 	const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-	// The first audio IO layout is used as the default. The other layouts may be selected either
-	// explicitly or automatically by the host or the user depending on the plugin API/backend.
 	const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
 		main_input_channels: NonZeroU32::new(2),
 		main_output_channels: NonZeroU32::new(2),
@@ -102,9 +90,6 @@ impl Plugin for Whammy {
 		aux_input_ports: &[],
 		aux_output_ports: &[],
 
-		// Individual ports and the layout as a whole can be named here. By default these names
-		// are generated as needed. This layout will be called 'Stereo', while a layout with
-		// only one input and output channel would be called 'Mono'.
 		names: PortNames::const_default(),
 	}];
 
@@ -113,13 +98,7 @@ impl Plugin for Whammy {
 
 	const SAMPLE_ACCURATE_AUTOMATION: bool = true;
 
-	// If the plugin can send or receive SysEx messages, it can define a type to wrap around those
-	// messages here. The type implements the `SysExMessage` trait, which allows conversion to and
-	// from plain byte buffers.
 	type SysExMessage = ();
-	// More advanced plugins can use this to run expensive background tasks. See the field's
-	// documentation for more information. `()` means that the plugin does not have any background
-	// tasks.
 	type BackgroundTask = ();
 
 	fn params(&self) -> Arc<dyn Params> {
@@ -132,16 +111,11 @@ impl Plugin for Whammy {
 		_buffer_config: &BufferConfig,
 		context: &mut impl InitContext<Self>,
 	) -> bool {
-		// Resize buffers and perform other potentially expensive initialization operations here.
-		// The `reset()` function is always called right after this function. You can remove this
-		// function if you do not need it.
+    // TODO: allocate here
 		true
 	}
 
-	fn reset(&mut self) {
-		// Reset buffers and envelopes here. This can be called from the audio thread and may not
-		// allocate. You can remove this function if you do not need it.
-	}
+
 
 	fn process(
 		&mut self,
@@ -149,27 +123,23 @@ impl Plugin for Whammy {
 		_aux: &mut AuxiliaryBuffers,
 		_context: &mut impl ProcessContext<Self>,
 	) -> ProcessStatus {
-		// WRITE OUT
 		for channel_samples in buffer.iter_samples() {
-			// Smoothing is optionally built into the parameters themselves
-			// let gain = self.params.gain.smoothed.next();
+      let delayed_read = self.write_pos as f32 - self.params.dive.smoothed.next() * 1000.;
+      let wrapped = if delayed_read < 0. {
+        self.buffer.len() as f32 + delayed_read // TODO: store buffer len as float in struct
+      } else {
+        delayed_read % self.buffer.len() as f32
+      };
 
+      let interpolated = self.interpolate(&wrapped, &self.buffer);
 			for sample in channel_samples {
         self.buffer[self.write_pos] = sample.clone();
-        
-        let delayed_read = self.write_pos as f32 - self.params.dive.smoothed.next() * 1000.;
-        let wrapped = if delayed_read < 0. {
-          self.buffer.len() as f32 + delayed_read // TODO: store buffer len as float in struct
-        } else {
-          delayed_read % self.buffer.len() as f32
-        };
-
-        let interpolated = self.interpolate(&wrapped, &self.buffer);
+        // TODO: Actually store each channel sample (currently overwriting)
         *sample = interpolated;
-        
-        self.write_pos = (self.write_pos + 1) % self.buffer.len();
-        self.envelope_pos = (self.envelope_pos + 1) % self.envelope.len();
 			}
+
+      self.write_pos = (self.write_pos + 1) % self.buffer.len();
+      self.envelope_pos = (self.envelope_pos + 1) % self.envelope.len();
 		}
 
 		ProcessStatus::Normal
